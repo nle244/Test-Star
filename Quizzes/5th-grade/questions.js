c938268d8b374d1c9dc509adc3d78f0f7ff1e0c1
SamEducational <89948811+SamEducational@users.noreply.github.com>
2022-03-02 21:50:51 -0800

    Add files via upload
    5th grade questions and background appended

--- Quizzes/5th-grade/questions.js
+++ Quizzes/5th-grade/questions.js
@@ -1,14 +1,10 @@
-//Format is Question, choice 1, choice 2, choice 3, choice 4, # of correct answer
+// Format is Question, choice 1, choice 2, choice 3, choice 4, # of correct answer.
 
 var questions = [
-  ["The Moon is seen because____", "sunlight reflects off of its surface", "the stars shine on it at night", "The Moon's surface is hot","The Moon makes its own light", "1"],
-  ["A gas giant planet is not made of solid matter. A terrestrial planet is made of___ ", "moons and rocks", "rocks and metals", "gases and heat", "metals and moons","2"],
-  ["How many planets are in our solar system?", "9", "6", "7", "8","4"],
-  [" What two planets are in between the Sun and Earth?", "Mars and Jupiter", "Mercury and Venus", "Neptune and Mercury", "Saturn and Uranus","2"],
-  ["The Moon revolves around___", "the Sun", " an eclipse", "the Earth", "another moon","3"],
-  ["Earth revolves around the ___", "Sun", "Moon", "clouds", "planets","1"],
-  ["How long does it take to go through the phases of the Moon cycle?", "About one day", "About one decade", "About one year", "About one month","4"],
-  ["Which item in space is an average-sized yellow star that is about 1.39 million  kilometers in diameter?", "Moon", "Sun", "Earth", "Jupiter","2"],
-  ["Which two scientists developed a model of the solar system most like the one used  today?", "Copernicus and Galileo", "Galileo and Aristotle", "Copernicus and Ptolemy", "Aristotle and Ptolemy","1"],
-  ["What is the correct order of the planets starting with the planet closest to the sun?", "Neptune, Uranus, Saturn, Jupiter, Mars, Earth, Venus, Mercury", "Mercury, Saturn, Earth, Jupiter, Venus, Neptune, Mars, Uranus", "Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune", "Mars, Uranus, Earth, Neptune, Saturn, Jupiter, Mercury, Venus","3"],
+  ["What does a polynomial look like?", "10", "x", "[x^2+6x+6]","4/10", "3"],
+  ["What is the answer to [9/0]?", "9", "Undefined due to division by zero", "0", "-0","2"],
+  ["What does Algebra deal with?", "Food recipes", "A How-To guide on doing things correctly", "It is a coding language", "Calculating and resolving things that have to do with numbers","4"],
+  ["What resembles the trajectory of a parabola?", "Fireworks going up in an arc-fashion", "Fireworks going straight up in the air", "A car going backwards", "All of the above","1"],
+  ["What math is this quiz about?", "Calculus", "Algebra", "Trigonometry", "All of the above","2"],
+  ["Algebra is said to be", "Step-by-step based to get to a solution", "Calculus", "Dealing with the geometry of shapes", "None of the above","1"],
   ];
