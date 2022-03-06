6cb8ef604fe8ad8a2095f9faafb2d1ead7cb3080
ryanchen64 <47126861+ryanchen64@users.noreply.github.com>
2022-03-05 20:13:35 -0800

    Add files via upload

--- Quizzes/6th-grade/questions.js
+++ Quizzes/6th-grade/questions.js
@@ -1,14 +1,14 @@
 //Format is Question, choice 1, choice 2, choice 3, choice 4, # of correct answer
 
 var questions = [
-  ["The Moon is seen because____", "sunlight reflects off of its surface", "the stars shine on it at night", "The Moon's surface is hot","The Moon makes its own light", "1"],
-  ["A gas giant planet is not made of solid matter. A terrestrial planet is made of___ ", "moons and rocks", "rocks and metals", "gases and heat", "metals and moons","2"],
-  ["How many planets are in our solar system?", "9", "6", "7", "8","4"],
-  [" What two planets are in between the Sun and Earth?", "Mars and Jupiter", "Mercury and Venus", "Neptune and Mercury", "Saturn and Uranus","2"],
-  ["The Moon revolves around___", "the Sun", " an eclipse", "the Earth", "another moon","3"],
-  ["Earth revolves around the ___", "Sun", "Moon", "clouds", "planets","1"],
-  ["How long does it take to go through the phases of the Moon cycle?", "About one day", "About one decade", "About one year", "About one month","4"],
-  ["Which item in space is an average-sized yellow star that is about 1.39 million  kilometers in diameter?", "Moon", "Sun", "Earth", "Jupiter","2"],
-  ["Which two scientists developed a model of the solar system most like the one used  today?", "Copernicus and Galileo", "Galileo and Aristotle", "Copernicus and Ptolemy", "Aristotle and Ptolemy","1"],
-  ["What is the correct order of the planets starting with the planet closest to the sun?", "Neptune, Uranus, Saturn, Jupiter, Mars, Earth, Venus, Mercury", "Mercury, Saturn, Earth, Jupiter, Venus, Neptune, Mars, Uranus", "Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune", "Mars, Uranus, Earth, Neptune, Saturn, Jupiter, Mercury, Venus","3"],
+  ["8.673 + 4.952", "16.345", "13.625", "12.345","15.8", "2"],
+  ["Solve for x, when 10 - x = 3", "x = 8", "x = 3", "x = 7", "x = 1","3"],
+  ["Find the area of a triangle with a base of 5 cm and a height of 8 cm", "35 cm²", "50 cm²", "16 cm²", "20 cm²","4"],
+  ["Calculate the mean of these 4 numbers: 5, 7, 3, 9", "6", "8", "4", "5","1"],
+  ["How else can you write 10⁴", "10 x 10 x 10 x 10", " 104", "1000000", "1000000","1"],
+  ["Round 131,294 to the nearest hundred", "131,000", "132,000", "131,205", "131,300","4"],
+  ["8/6 + 7/12? (in fraction)", "12/17", "23/12", "18/17", "21/12","2"],
+  ["What is the circumference of a circle with a diameter of 5.5 feet?(Use π = 3.14)", "17.27", "18.14", "20.45", "7.065","1"],
+  ["If Jack has a snake that's 240 inches long. How long is that in feet?", "12.6 feet", "14 feet", "26.2 feet", "20 feet","4"],
+  ["What is the area of a rectangle with a base of 6 cm and a height of 8 cm?", "54 cm²", "46 cm²", "48 cm²", "56 cm²","3"],
   ];
